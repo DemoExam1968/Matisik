1a17afb2b5ed5d4c4d0dc9c1864ba26e29a88519
DemoExam1968 <114480888+DemoExam1968@users.noreply.github.com>
2022-09-27 12:51:16 +0300

    Данные для Контроля1

---
+++ Сессия1/mssql_script_trade_users.sql
@@ -0,0 +1,20 @@
+create database [Trade]
+go
+use [Trade]
+go
+create table [Role]
+(
+	RoleID int primary key identity,
+	RoleName nvarchar(100) not null
+)
+go
+create table [User]
+(
+	UserID int primary key identity,
+	UserSurname nvarchar(100) not null,
+	UserName nvarchar(100) not null,
+	UserPatronymic nvarchar(100) not null,
+	UserLogin nvarchar(max) not null,
+	UserPassword nvarchar(max) not null,
+	UserRole int foreign key references [Role](RoleID) not null
+)
