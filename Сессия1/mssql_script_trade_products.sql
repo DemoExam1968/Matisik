efd9766890014b99bbf28271a2af0c60b402deee
DemoExam1968 <114480888+DemoExam1968@users.noreply.github.com>
2022-10-04 15:58:27 +0300

    Add files via upload
    Добавлены файлы для работы со списком товаров

---
+++ Сессия1/mssql_script_trade_products.sql
@@ -0,0 +1,16 @@
+use [DBCooker]
+go
+
+create table Product
+(
+	ProductArticleNumber nvarchar(100) primary key,
+	ProductName nvarchar(max) not null,
+	ProductDescription nvarchar(max) not null,
+	ProductCategory nvarchar(max) not null,
+	ProductPhoto image not null,
+	ProductManufacturer nvarchar(max) not null,
+	ProductCost decimal(19,4) not null,
+	ProductDiscountAmount tinyint null,
+	ProductQuantityInStock int not null,
+	ProductStatus nvarchar(max) not null,
+)
